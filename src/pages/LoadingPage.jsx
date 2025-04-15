10524817a12cea3874b29c1614b922b62b0b0831
PeterMassmann <pedro@massmann.cl>
2025-04-15 11:54:28 -0400

    Added TailwindCSS (v3 for Webstorm support)

---
+++ src/pages/LoadingPage.jsx
@@ -0,0 +1,9 @@
+const LoadingPage = () => {
+    return <>
+        <p1 className={"text-center text-red-500 text-3xl font-bold"}>
+            Hello world!
+        </p1>
+    </>;
+}
+
+export default LoadingPage;
