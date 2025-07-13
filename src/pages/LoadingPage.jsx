ae9d73a41c76a86af5676bc173c816c0569b2631
bernirosas <bernardita.rosas@uc.cl>
2025-07-12 21:43:08 -0400

    feat: navbar

--- src/pages/LoadingPage.jsx
+++
@@ -1,9 +0,0 @@
-const LoadingPage = () => {
-    return <>
-        <p1 className={"text-center text-red-500 text-3xl font-bold"}>
-            Hello world!
-        </p1>
-    </>;
-}
-
-export default LoadingPage;
--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -1,43 +1,47 @@
 import MapLayer from "../components/map/layers/MapLayer.jsx";
 import MapDisplay from "../components/map/MapDisplay.jsx";
 import OverlayLayer from "../components/map/layers/OverlayLayer.jsx";
+import Navbar from "../components/common/Navbar.jsx";
 
 const MapPage = () => {
 
     return (
-        <MapDisplay
-            topLeft={{lon: -70.75860376620324, lat:-33.5314107698844}}
-            bottomRight={{lon: -70.64506335632365, lat: -33.59220524202586}}
-            initialZoom={15}
-            minZoom={14}
-            maxZoom={19}
-            classNames={{
-                container: "m-auto w-[100vw] h-[100vh]",
-            }}
-            mapId={0}
-        >
-            <MapLayer />
-            <OverlayLayer
-                z={10}
-                shapes={[
-                    {
-                        id: 1,
-                        type: "poly",
-                        coordinates: [
-                            {lat: -33.56705874709786, lon: -70.67740632305711},
-                            {lat: -33.57828671310991, lon: -70.66778819911015},
-                            {lat: -33.57828671310991, lon: -70.67740632305711},
-                            {lat: -33.55828671310991, lon: -70.687406323057}
-                        ],
-                        hoverText: "Parque Forestal",
-                        color: {
-                            fill: "rgba(255,0,59,0.25)",
-                            stroke: "rgba(255,0,59,1)"
+        <>
+        <Navbar />
+            <MapDisplay
+                topLeft={{lon: -70.75860376620324, lat:-33.5314107698844}}
+                bottomRight={{lon: -70.64506335632365, lat: -33.59220524202586}}
+                initialZoom={15}
+                minZoom={14}
+                maxZoom={19}
+                classNames={{
+                    container: "m-auto w-[100vw] h-[100vh] mt-14",
+                }}
+                mapId={0}
+            >
+                <MapLayer />
+                <OverlayLayer
+                    z={10}
+                    shapes={[
+                        {
+                            id: 1,
+                            type: "poly",
+                            coordinates: [
+                                {lat: -33.56705874709786, lon: -70.67740632305711},
+                                {lat: -33.57828671310991, lon: -70.66778819911015},
+                                {lat: -33.57828671310991, lon: -70.67740632305711},
+                                {lat: -33.55828671310991, lon: -70.687406323057}
+                            ],
+                            hoverText: "Parque Forestal",
+                            color: {
+                                fill: "rgba(255,0,59,0.25)",
+                                stroke: "rgba(255,0,59,1)"
+                            }
                         }
-                    }
-                ]}
-            />
-        </MapDisplay>
+                    ]}
+                />
+            </MapDisplay>
+        </>
     )
 }
 
