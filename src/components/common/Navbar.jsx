2f6905725cdf15506a8d69a0bdcb9ff6fcf4d2ac
bernirosas <bernardita.rosas@uc.cl>
2025-07-12 21:46:45 -0400

    fix:center x axis

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav className="bg-primary py-2 shadow-md items-start fixed top-0 left-0 w-full z-50">
-            <div className="max-w-5xl ml-10 flex items-start px-5">
+        <nav className="bg-primary py-2 shadow-md items-center justify-center fixed top-0 left-0 w-full z-50">
+            <div className="max-w-5xl ml-10 flex items-center justify-start align-items-center px-5">
                 <img
                     src="/logo_el_bosque.png"
                     alt="Logo"
